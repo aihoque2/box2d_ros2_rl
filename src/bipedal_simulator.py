825c1527ac57115f7db9dcc85542eb8c17942a2a
aihoque2 <aihoque2@illinois.edu>
2024-03-11 01:42:17 -0700

    action_callback work for bipedal simulation

--- src/bipedal_simulator.py
+++ src/bipedal_simulator.py
@@ -3,6 +3,7 @@ from rclpy import Node
 import gym
 import numpy as np
 
+from std_msgs import Float64MultiArray, Float64Array
 """
 bipedal_simulator.py
 
@@ -16,4 +17,29 @@ class BipedalSim(Node):
             '/action',
             self.action_callback,
             10)
-        self.env = gym.make("BipedalWalker-v2", render_mode="human")
+        
+        self.state_pub = rclpy.create_publisher(Float64MultiArray, "state", 10)
+        self.timer_period = 0.5 # seconds
+
+        self.env = gym.make("BipedalWalker-v3", render_mode="human")
+        self.state_size = self.env.observation_space.shape[0]
+        self.action_size = self.env.action_space
+        self.state, info = self.env.reset()
+    
+    def action_callback(self, msg: Float64Array):
+        """
+        send the force message and update
+        """
+
+        action = np.array(msg)
+        new_state, reward, terminated, truncated, info = self.env.step(action)
+        
+        # TODO: do something with the reward and other vars
+        self.state = new_state
+        
+        if terminated or truncated:
+            state, info = self.env.reset()
+            self.state = state
+    
+    def timer_callback(self):
+        pass
