00b7ed8f2844fd1d3b9472589250b7746701dd1b
aihoque2 <aihoque2@illinois.edu>
2024-03-26 02:25:01 -0700

    put gymnasium instead of gym so i can be up-to-date like the cool kids

--- src/bipedal_simulator.py
+++ src/bipedal_simulator.py
@@ -1,24 +1,30 @@
 import rclpy
 from rclpy import Node
-import gym
+import gymnasium as gym
 import numpy as np
 
-from std_msgs import Float64MultiArray, Float64Array
+from std_msgs.msg import Float64MultiArray, Float64
 """
 bipedal_simulator.py
 
-node that handles the simulation
+node that handles the simulation of bipedalWalker
+
+this is just proof of concept of reinforcement learning
+using controllers implemented with ROS2.
 """
 
 class BipedalSim(Node):
     def __init__(self):
+        super().__init__('bipedal_sim')
+
         self.action_sub = rclpy.create_subscription(
             #Action_Message,
             '/action',
             self.action_callback,
             10)
         
-        self.state_pub = rclpy.create_publisher(Float64MultiArray, "state", 10)
+        self.state_pub = rclpy.create_publisher(Float64MultiArray, "/state", 10)
+        self.reward_pub = rclpy.create_publisher(Float64, "/reward", 10)
         self.timer_period = 0.5 # seconds
 
         self.env = gym.make("BipedalWalker-v3", render_mode="human")
@@ -26,7 +32,7 @@ class BipedalSim(Node):
         self.action_size = self.env.action_space
         self.state, info = self.env.reset()
     
-    def action_callback(self, msg: Float64Array):
+    def action_callback(self, msg: Float64MultiArray):
         """
         send the force message and update
         """
@@ -36,10 +42,12 @@ class BipedalSim(Node):
         
         # TODO: do something with the reward and other vars
         self.state = new_state
-        
+        state_msg = Float64MultiArray(self.state)
+        self.state_pub.publish(state_msg)
+
         if terminated or truncated:
             state, info = self.env.reset()
             self.state = state
+            state_msg = Float64MultiArray(self.state)
+            self.state_pub.publish(state_msg)
     
-    def timer_callback(self):
-        pass
